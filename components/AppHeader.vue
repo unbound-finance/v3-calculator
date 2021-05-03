fa3e0971fe5397c5883aef4a5ff991c28a09ce63
Saurabh Bachawe <sb4514.work@gmail.com>
2021-05-03 09:20:14 +0530

    ft: logo

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,7 +3,10 @@
     <div class="flex items-center justify-center flex-1 mx-auto max-w-6xl">
       <div class="w-full flex items-center justify-between px-4">
         <nuxt-link to="/">
-          <span>Unbound</span>
+          <div class="flex items-center space-x-2">
+            <img src="~/assets/unbound.svg" alt="unbound" width="24" />
+            <span>Unbound</span>
+          </div>
         </nuxt-link>
         <div class="flex items-center space-x-4">
           <nuxt-link to="/faucet" class="text-sm">Faucet</nuxt-link>
