b09d9b3f490410488c7d94c0ba9468d6e7980a9e
Saurabh Bachawe <sb4514.work@gmail.com>
2021-05-04 14:17:28 +0530

    ft: bump new addresses

--- constants/index.js
+++ constants/index.js
@@ -7,9 +7,9 @@ const FaucetABI = require('./abis/Faucet.json')
 export const NFTPositionManagerAddress =
   '0x815BCC87613315327E04e4A3b7c96a79Ae80760c'
 
-export const daiAddress = '0x88254521B0706D59C407B1578576D91dfE2E86Db'
-export const wethAddress = '0xeee3c7c2873d1bc87a6a6374827c0b75ead656ee'
-export const ethDaiPoolAddress = '0xf3E21d36352167e5c4C8F26186fF69d3e946105a'
+export const daiAddress = '0xF821ddD9068B75F0d906f066590c646085B9fF3a'
+export const wethAddress = '0xf35acA6667C80705fFB21a84ab39F1ac9a4b00fE'
+export const ethDaiPoolAddress = '0x95f917bD85d0aa741bC8e2AC6b69e8427c991F5b'
 export const faucetAddress = '0x1EADf7c1428b91239D9a51dD3e895eEA13B033fC'
 
 export const MaxUint128 = BigNumber.from(2).pow(128).sub(1)
